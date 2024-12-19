c1d6c729f534e757883eb3daf3f9035a8bf15a32
Filip Andrejuk <filip_andrejuk@hotmail.com>
2024-12-19 17:17:37 -0500

    More DOM manipulation

--- javascript-review.js
+++ javascript-review.js
@@ -1,11 +1,20 @@
 let string = "Words";
 let int = 0;
+let animalInt = 2;
+let animal = "Lion";
 
-function updateParagraph() {
+function updateInt() {
   int++;
   document.getElementById("paragraph").innerHTML = int;
 }
 
+function updateAnimal() {
+  animalInt < 2 ? animalInt++ : (animalInt = 0);
+  animal = animalInt == 0 ? "Lion" : animalInt == 1 ? "Tiger" : "Bear";
+  document.getElementById("animal").innerHTML = animal;
+}
+
 let button = document.getElementById("button");
 
-button.addEventListener("click", updateParagraph);
+button.addEventListener("click", updateInt);
+button.addEventListener("click", updateAnimal);
